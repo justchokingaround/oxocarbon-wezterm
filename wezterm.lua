4c7131457b97f3b1872787402524791bde34ec22
justchokingaround <ivanonarch@tutanota.com>
2023-01-13 20:20:21 +0100

    init

---
+++ wezterm.lua
@@ -0,0 +1,33 @@
+return {
+	colors = {
+		foreground = "#dde1e6",
+		background = "#161616",
+		cursor_bg = "#f2f4f8",
+		cursor_fg = "#393939",
+		cursor_border = "#f2f4f8",
+		selection_fg = "#f2f4f8",
+		selection_bg = "#525252",
+		scrollbar_thumb = "#222222",
+		split = "#444444",
+		ansi = {
+			"#262626",
+			"#ff7eb6",
+			"#42be65",
+			"#82cfff",
+			"#33b1ff",
+			"#ee5396",
+			"#3ddbd9",
+			"#dde1e6",
+		},
+		brights = {
+			"#393939",
+			"#ff7eb6",
+			"#42be65",
+			"#82cfff",
+			"#33b1ff",
+			"#ee5396",
+			"#3ddbd9",
+			"#ffffff",
+		},
+	},
+}
